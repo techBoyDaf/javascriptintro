5f53d62a0f6369d293d6cd7a4ce4598dc1b0a28e
Edafe ozumu <edafeozumu@gmail.com>
2022-06-21 18:27:02 +0100

    code for task

---
+++ script.js
@@ -0,0 +1,4 @@
+document.write("<h1> This is a brief introduction about me </h1>");
+document.write("<h3>Full Name: Ozumu Edafe </h3>");
+document.write("<h3>Height: 178cm</h3>");
+document.write("<h3>Country: Nigeria</h3>");
